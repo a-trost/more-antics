2b00746c8ced6eac43d24a98eaff656bbaa58c16
Alex Trost <19617280+a-trost@users.noreply.github.com>
2021-07-09 11:59:14 -0400

    Fix text color

--- components/LinkButton/index.js
+++ components/LinkButton/index.js
@@ -4,7 +4,7 @@ const Button = ({ children, href, className }) => {
   return (
     <a
       href={href}
-      className={`relative inline-flex bg-ternary-glare justify-center px-4 py-2 text-base font-bold leading-normal text-center cursor-pointer font-display hover:bg-transparent border-8 border-ternary-glare ${className}`}
+      className={`text-black relative inline-flex bg-ternary-glare justify-center px-4 py-2 text-base font-bold leading-normal text-center cursor-pointer font-display hover:bg-transparent border-8 border-ternary-glare ${className}`}
     >
       {children}
     </a>
